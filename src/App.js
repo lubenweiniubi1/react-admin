f4f6b5df70b360790940974277cd8f6ff4560985
潘林峰 <631912579@qq.com>
2020-08-23 15:54:59 +0800

    配置内层路由

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
 import React, { Component } from "react"
 import { Button } from "antd"
 
-const testHOC = (WrappedComponent) => {
-  return class HOCComponent extends Component {
-    render() {
-      return (
-        <>
-          <WrappedComponent></WrappedComponent>
-          <div>这是高阶组件的信息</div>
-        </>
-      )
-    }
-  }
-}
+import { Route, Switch, Redirect } from "react-router-dom"
+import { adminRouter } from "./routes"
 
-@testHOC
 class App extends Component {
   render() {
     return (
       <div>
-        <Button type="primary">你好</Button>
+        <div>公共部分</div>
+        <Switch>
+          {adminRouter.map((route) => {
+            return (
+              <Route
+                key={route.pathname}
+                path={route.pathname}
+                exact={!!route.exact}
+                render={(routerProps) => {
+                  return <route.component {...routerProps}></route.component>
+                }}
+              ></Route>
+            )
+          })}
+          <Redirect to={adminRouter[0].pathname} from="/admin" exact></Redirect>
+          <Redirect to='/404'  ></Redirect>
+        
+        </Switch>
       </div>
     )
   }
 }
-//看没配置时是否能用
-// export default testHOC(App)
 
 export default App
