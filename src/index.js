0d99f667ffd57164c0088e24b35692f971a020c4
潘林峰 <631912579@qq.com>
2020-08-23 15:38:35 +0800

    项目结构搭建和外层路由配置

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 import React from "react"
 import { render } from "react-dom"
 
+import { mainRouter } from "./routes"
+
+import { HashRouter as Router, Route, Switch, Redirect } from "react-router-dom"
+
 import App from "./App"
 
 import "./index.less"
-render(<App />, document.querySelector("#root"))
+render(
+  <Router>
+    <Switch>
+      <Route
+        path="/admin"
+        render={(routeProps) => {
+          //todo：权限，需要登陆才能访问/admin
+          return <App {...routeProps} />
+        }}
+      ></Route>
+      {mainRouter.map((route) => {
+        return (
+          <Route
+            key={route.pathname}
+            path={route.pathname}
+            component={route.component}
+          />
+        )
+      })}
+      <Redirect to="/admin" exact from="/"></Redirect>
+      <Redirect to="/404"></Redirect>
+    </Switch>
+  </Router>,
+  document.querySelector("#root")
+)
---
+++ src/views/index.js
@@ -0,0 +1,8 @@
+import Dashboard from "./Dashboard"
+import Settings from "./Settings"
+import ArticleList from "./Article"
+import ArticleEdit from "./Article/Edit"
+import Login from "./Login"
+import NotFound from "./NotFound"
+
+export { Dashboard, Settings, ArticleEdit, ArticleList, Login, NotFound }
